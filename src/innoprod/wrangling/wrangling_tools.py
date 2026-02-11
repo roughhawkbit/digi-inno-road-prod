edfa43fdf225c842480d85d83b5b25d798b648b8
Robert Clegg <roughhawkbit@gmail.com>
2026-02-11 11:51:33 +0000

    Identify & count rows with complete core data

--- src/innoprod/wrangling/wrangling_tools.py
+++ src/innoprod/wrangling/wrangling_tools.py
@@ -8,8 +8,7 @@ def replace_values(df, colname, old_value, new_value):
 def characterise_df_columnwise(df):
     characterisation = []
     for col in df.columns:
-        # Values cannot be both '' and NaN, so this mask is True only when both are false
-        mask = (df[col] == '') == (df[col].isna())
+        mask = is_non_empty(df[col])
         u = df[mask][col].unique().size
         c = sum(mask)
         N = df[col].size
@@ -29,5 +28,8 @@ def characterise_df_columnwise(df):
 def is_in_date_format(series: pd.Series):
     if not pd.api.types.is_string_dtype(series):
         return False
-    
+
+def is_non_empty(series: pd.Series):
+    # Values cannot be both '' and NaN, so this mask is True only when both are false
+    return (series == '').fillna(False) == (series.isna())
     
