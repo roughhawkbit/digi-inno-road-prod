d7935abc9383924b1be60e00bed87373c0f739fe
Robert Clegg <roughhawkbit@gmail.com>
2026-04-21 14:50:19 +0100

    Tools for chunking text

---
+++ innoprod/text_analysis/chunking_tools.py
@@ -0,0 +1,34 @@
+from collections import OrderedDict
+import math
+from nltk.tokenize import PunktSentenceTokenizer, RegexpTokenizer
+
+
+def split_token_dict(token_dict: OrderedDict, max_len: int):
+  if max(token_dict.values()) > max_len:
+    raise Exception(f"Largest sentence token is {max(token_dict.values())} word tokens long, greater than the permitted maximum of {max_len}.")
+  if len(token_dict) < 2:
+    return [token_dict]
+  total_len = sum(token_dict.values())
+  min_n_chunks = math.ceil(total_len / max_len)
+  target_chunk_len = int(round(total_len / min_n_chunks, 0))
+  new_dict = OrderedDict()
+  counter = 0
+  while counter < target_chunk_len:
+    if next(iter(token_dict.values())) + counter > max_len:
+      break
+    sentence, token_len = token_dict.popitem(last=False)
+    counter += token_len
+    new_dict[sentence] = token_len
+  return [new_dict] + split_token_dict(token_dict, max_len)
+  
+
+def chunk_text_sentencewise(text, max_words):
+  sent_tokenizer = PunktSentenceTokenizer()
+  word_tokenizer = RegexpTokenizer(r'\w+')
+  sentence_tokens = sent_tokenizer.tokenize(text)
+  od = OrderedDict()
+  for sentence in sentence_tokens:
+    od[sentence] = len(word_tokenizer.tokenize(sentence))
+  new_dicts = split_token_dict(od, max_words)
+  return [" ".join(list(d.keys())) for d in new_dicts]
+
---
+++ tests/text_analysis/test_chunking_tools.py
@@ -0,0 +1,53 @@
+
+import unittest
+from parameterized import parameterized
+
+from collections import OrderedDict
+
+from innoprod.text_analysis import chunking_tools
+
+one_sentence = OrderedDict()
+one_sentence['Sentence one.'] = 2
+
+two_sentences = OrderedDict()
+two_sentences['Sentence one.'] = 2
+two_sentences['Sentence two.'] = 2
+
+three_sentences = OrderedDict()
+three_sentences['Sentence one.'] = 2
+three_sentences['Sentence two.'] = 2
+three_sentences['Sentence three.'] = 2
+
+second_sentence = OrderedDict()
+second_sentence['Sentence two.'] = 2
+
+third_sentence = OrderedDict()
+third_sentence['Sentence three.'] = 2
+
+class TestChunkingTools(unittest.TestCase):
+
+    @parameterized.expand([
+        [one_sentence, 3, [one_sentence]],
+        [two_sentences, 3, [one_sentence, second_sentence]],
+        [three_sentences, 4, [two_sentences, third_sentence]],
+        [three_sentences, 5, [two_sentences, third_sentence]],
+    ])
+    def test_split_token_dict(self, input_dict, max_len, expected_output):
+        # Arrange
+        # Act
+        output = chunking_tools.split_token_dict(input_dict, max_len)
+        # Assert
+        self.assertEqual(output, expected_output)
+
+    @parameterized.expand([
+        ["Sentence one. Sentence two. Sentence three.", 5, ["Sentence one. Sentence two.", "Sentence three."]],
+        ["Sentence one. Sentence two. Sentence three.", 4, ["Sentence one. Sentence two.", "Sentence three."]],
+        ["Sentence one. Sentence two. Sentence three.", 3, ["Sentence one.", "Sentence two.", "Sentence three."]],
+    ])
+    def test_chunk_text_sentencewise(self, input_text, max_words, expected_output):
+        # Arrange
+        # Act
+        output = chunking_tools.chunk_text_sentencewise(input_text, max_words)
+        # Assert
+        self.assertEqual(output, expected_output)
+
