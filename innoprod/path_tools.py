e8bcea9c84838f90f1e6bcfd8a4fee4ac708f39d
Robert Clegg <roughhawkbit@gmail.com>
2026-05-22 17:00:09 +0100

    Add path tool for finding highest numbered subdirectory

--- innoprod/path_tools.py
+++ innoprod/path_tools.py
@@ -1,4 +1,17 @@
-import os.path
+import os
+
+
+def find_highest_numbered_subdir(directory):
+  subdirs = os.listdir(directory)
+  num = 0
+  highest = None
+  for subdir in subdirs:
+    s_num = int(subdir.split('-')[-1])
+    if s_num > num:
+      num = s_num
+      highest = subdir
+  return highest
+
 
 def secrets_path():
   p = os.path.abspath(__file__)
