831459a01872c176d29486d7134844c0f857b103
Robert Clegg <roughhawkbit@gmail.com>
2026-05-24 10:01:26 +0100

    Add DRS as an importable file

---
+++ innoprod/digital_readiness_score.py
@@ -0,0 +1,14 @@
+# Digital Readiness Score (DRS) levels: taken from Table 6 of "PAS 1040:2019 Digital Technologies in Manufacturing" (British Standards Institution, 2019)
+# https://www.bsigroup.com/en-GB/insights-and-media/insights/brochures/pas-1040-adopting-digital-technologies-in-manufacturing-guide/
+
+DRS_LEVELS = [
+    "The business has no vision for driving growth with digital technologies, and is not supporting workers to investigate opportunities.", # 1
+    "Workers are aware of the potential for digital technologies and are supported by the business to experiment with local trials.", # 2
+    "The business is learning from local trials of digital technologies and leaders are investigating the business case.", # 3
+    "The business has a strategic vision for digital transformation, the business case is agreed and implementation is underway.", # 4
+    "Workers are engaged in digital transformation and the business is starting to achieve business case benefits.", # 5
+    "Digital technologies are driving continuous improvement in key aspects of operational performance including supply chain and customer services.", # 6
+    "Innovation with digital technologies is part of the culture of the business and is driving continuous improvement in all aspects.", # 7
+    "Increasing adoption of digital technologies is sustained by reinvestment of related profits and continuous renewal of the business case.", # 8
+    "Digital technologies are driving optimized productivity and competitiveness for the business and its partners.", # 9
+]
